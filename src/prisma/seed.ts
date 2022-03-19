f4dbfc1e405ce731d21bde83be835a8c287e51f3
Nico Lopez <nicolopez413@gmail.com>
2022-03-19 10:59:08 +0100

    Fixed prisma/seed imports

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Event, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 import filteredShows from './mocks/mockShows'
 
