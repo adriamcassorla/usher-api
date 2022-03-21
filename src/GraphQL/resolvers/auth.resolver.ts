a3a94f13f699a70a5535891a6a46dd9c07302483
Nico Lopez <nicolopez413@gmail.com>
2022-03-21 12:41:03 +0100

    Creates users with encrypted password, retrieves token on login

--- src/GraphQL/resolvers/auth.resolver.ts
+++ src/GraphQL/resolvers/auth.resolver.ts
@@ -2,9 +2,9 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 const SECRET_KEY = process.env.SECRET_KEY as string;
 
-export const createUser =async (_, args : {email: string, password: string}, ctx) => {
+export const createUser =async (_, args : {email: string, password: string, first_name: string, last_name: string, notifications: boolean}, ctx) => {
 
-  const { email, password } = args;
+  const { email, password, first_name, last_name, notifications } = args;
   if(!email || !password) {
     console.error('Provide valid email and password');
     return
@@ -21,17 +21,22 @@ export const createUser =async (_, args : {email: string, password: string}, ctx
   try {
 
     const hash = await bcrypt.hash(password, 10);
+    console.log(hash)
     const newUser = await ctx.prisma.user.create({
       data: {
         email,
         password: hash,
-      }
+        first_name,
+        last_name,
+        notifications
+      },
     })
+    console.log(newUser)
     const accessToken = jwt.sign({ id: newUser.id, role: 'user' }, SECRET_KEY, {expiresIn: '10h'})
     return accessToken
   } catch (e) {
-    console.error('Unsuccesful to generate new user');
-    return
+    console.error(e);
+    return 'Unsuccesful to generate new user'
   }
 }
 
@@ -39,7 +44,7 @@ export const login = async (_, args: {email: string, password: string}, ctx) => 
   const { email, password } = args;
   try {
 
-    const user = await ctx.prisma.user.findUnique({ where: email })
+    const user = await ctx.prisma.user.findUnique({ where: { email } })
     const validatePass = bcrypt.compare(password, user.password)
     if (!validatePass) {
       throw new Error('Invalid user or password')
@@ -47,8 +52,8 @@ export const login = async (_, args: {email: string, password: string}, ctx) => 
     const accessToken = jwt.sign({ id: user.id, role: 'user' }, SECRET_KEY, {expiresIn: '10h'})
     return accessToken;
   } catch (e) {
-    console.error('Invalid user or password');
-    return
+    console.error(e);
+    return 'Invalid user or password'
   }
 
 }
