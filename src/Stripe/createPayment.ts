f5a5d490ccc2f07a36b32a488633b070427e8469
Nico Lopez <nicolopez413@gmail.com>
2022-03-26 12:40:01 +0100

    Improved payment process

--- src/Stripe/createPayment.ts
+++ src/Stripe/createPayment.ts
@@ -1,31 +1,18 @@
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_TEST_KEY || "sk_test_51KgrhQLqoAr5l9cwJOdNZ9MbmlcCmVPSgi4nDQMK6B3HPjhKxYq5dcGMoqlmJZEVDQgojSQKZbF1VsulVnXP5WuK00RQS2jlWj", {apiVersion: '2020-08-27', typescript: true}); 
+const STRIPE_TEST_KEY = process.env.STRIPE_TEST_KEY as string
+const stripe = new Stripe(STRIPE_TEST_KEY, {apiVersion: '2020-08-27', typescript: true}); 
 
 const createPayment = async (req, res) => {
-  // Use an existing Customer ID if this is a returning customer.
-  // const customer = await stripe.customers.create();
-  // const ephemeralKey = await stripe.ephemeralKeys.create(
-  //   {customer: customer.id},
-  //   {apiVersion: '2020-08-27'}
-  // );
-  console.log('trying to pay');
+  const { amount, currency } = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'eur',
-    // customer: customer.id,
+    amount,
+    currency,
     automatic_payment_methods: {
       enabled: true,
     },
   });
   const clientSecret = paymentIntent.client_secret
-  console.log('sending client secret')
   res.json({clientSecret})
-  // res.json({
-  //   paymentIntent: paymentIntent.client_secret,
-  //   ephemeralKey: ephemeralKey.secret,
-  //   customer: customer.id,
-  //   publishableKey: 'pk_test_51KgrhQLqoAr5l9cwCqC9ZvVhuT6aU7XT5tGYNo1fUI1jcJQ5lN7YP6JP7FEPx1zeOdZ1dIp5AM8UM2efBgSZyDFm00SCEdEK5b'
-  // });
 };
 
 export default createPayment;
